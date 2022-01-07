2411acbc0b25c14588688a9494dd457884313c8e
Pankaj Prajapati <paprajapati9@gmail.com>
2022-01-07 19:22:34 +0530

    multiple changes

--- src/app/util/timeline/timeline.component.ts
+++ src/app/util/timeline/timeline.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
-  constructor() { }
+  title: String;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.title = 'Timeline';
   }
 
 }
